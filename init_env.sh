b06e4949bdbc6a9996afae41cdb04f4f1c4671fc
root <root@vultr.guest>
2015-07-24 13:27:52 +0800

    update

--- init_env.sh
+++ init_env.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+date=`date +%Y%m%d`
+
+# for vim
+mv $HOME/.vimrc $HOME/.vimrc$date
+
+cp ./.vimrc $HOME/
+
+# for tmux
+mv $HOME/.tmux.conf $HOME/.tmux.conf$date
+
+cp .tmux.conf $HOME/
+
 # init vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
